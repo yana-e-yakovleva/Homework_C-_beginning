08b26cb1d5cd8e791ea83d0dbf2df0653fabfdd9
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-11-25 00:17:19 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int max;
+
+if (A > B )
+{max=A;}
+else{max=B;}
+
+if (max > C)
+{max=max;}
+
+else {max=C;}
+
+Console.WriteLine(max);
+
+
+
+
+
