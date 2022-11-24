25a96b6892ad97296d98a2fb99084cbe83d4ed13
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-11-24 23:14:10 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{Console.WriteLine(numberA);}
+
+else {Console.WriteLine(numberB);}
